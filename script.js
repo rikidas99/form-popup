ec092ad275bb3164cb9303e2bd9f765021f61af8
Hriteshdas99 <70994170+rikidas99@users.noreply.github.com>
2024-07-05 14:09:57 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('openFormButton').addEventListener('click', function() {
-    document.getElementById('formPopup').style.display = 'flex';
-});
-
-document.getElementById('closeFormButton').addEventListener('click', function() {
-    document.getElementById('formPopup').style.display = 'none';
-});
-
-
