a71c1786760559b9a5102ab4d36abfbf941560f8
Hriteshdas99 <70994170+rikidas99@users.noreply.github.com>
2024-07-05 13:58:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,20 +6,4 @@ document.getElementById('closeFormButton').addEventListener('click', function() 
     document.getElementById('formPopup').style.display = 'none';
 });
 
-document.getElementById('popupForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    let formData = new FormData(this);
-    
-    fetch('submit_form.php', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-        alert(data);
-        document.getElementById('formPopup').style.display = 'none';
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-});
+
